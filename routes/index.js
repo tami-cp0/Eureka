f70849c987ea314c54849aecff0489215c2181fe
tami-cp0 <findtamilore>
2024-08-28 00:47:26 +0100

    added more routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import UsersController from "../controllers/UserController.js";
 import AuthController from "../controllers/AuthController.js";
 import CoursesController from "../controllers/CourseController.js";
+import ChapterController from "../controllers/ChapterController.js";
 
 const router = Router()
 
@@ -19,52 +20,65 @@ router.post('/login', AuthController.connect, (req, res) => {
   res.redirect('/home');
 });
 
-router.get('/logout', AuthController.disconnect);
+router.get('/logout', AuthController.loginRequired, AuthController.disconnect);
 
 router.delete('/deleteUser', AuthController.loginRequired, UsersController.deleteMe);
 
-router.get('/', (req, res) => {
-  res.redirect('/publish');
+router.get('/', AuthController.loginRequired, (req, res) => {
+  res.redirect('/home');
 })
 
 
 // main routes
-router.get('/home', (req, res) => {
-  res.render('home');
+router.get('/home', AuthController.loginRequired, async (req, res) => {
+  const courses = await CoursesController.get10Courses(req, res);
+  res.render('home', { courses });
 });
 
-router.get('/publish', (req, res) => {
-  res.render('publish');
+router.get('/publish', AuthController.loginRequired, (req, res) => {
+  res.render('publish', { courseId: null });
 });
 
-router.use('/courses/browse', (req, res) => {
-  res.render('courses/browse');
+router.get('/publish/:courseId', AuthController.loginRequired, (req, res) => {
+  const courseId = req.params.courseId;
+  res.render('publish', { courseId });
 });
 
-router.use('/courses/ongoing', (req, res) => {
+router.get('/courses/browse', AuthController.loginRequired, async (req, res) => {
+  const courses = await CoursesController.get10Courses(req, res);
+  res.render('courses/browse', { courses });
+});
+
+router.get('/courses/ongoing', AuthController.loginRequired, (req, res) => {
   res.render('courses/ongoing');
 });
 
-router.use('/courses/published', (req, res) => {
-  res.render('courses/published');
-});
+router.get('/courses/published', AuthController.loginRequired, CoursesController.getPublished);
 
-router.use('/courses/drafts', (req, res) => {
-  res.render('courses/drafts');
-});
+router.get('/courses/drafts', AuthController.loginRequired, CoursesController.getDrafts);
+
+
+router.get('/getMoreCourses', AuthController.loginRequired, CoursesController.getMoreCourses);
 
 
 // routes related to information
-router.get('/user', UsersController.getMe);
+router.get('/user', AuthController.loginRequired, UsersController.getMe);
 
-router.post('/publish', CoursesController.postCourse);
+router.post('/publish', AuthController.loginRequired, CoursesController.postCourse);
 
-router.put('/publish/:id', CoursesController.putCourse);
+router.put('/publish/:courseId', AuthController.loginRequired, CoursesController.putCourse);
 
-router.get('course/:id/view', CoursesController.viewCourse);
+router.get('/course/:courseId/view', AuthController.loginRequired, CoursesController.viewCourse);
+// router.get('course/start/:id', CoursesController.viewCourse);
 
-router.get('courses/:id', CoursesController.getCourse);
-router.get('/courses', CoursesController.getAllCourses);
+// router.get('/course/:courseId', CoursesController.getCourse);
+router.delete('/deleteCourse/:courseId', AuthController.loginRequired, CoursesController.deleteCourse);
+
+
+// router.get('/courses', AuthController.loginRequired, CoursesController.getAllCourses);
+
+// get all chapters related to the courseId
+router.get('/course/:courseId/chapters', AuthController.loginRequired, ChapterController.getChapters);
 
 
 export default router;
