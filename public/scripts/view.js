b5a1d59c59e9cf6a07a36242f1eb47e80cffb479
tami-cp0 <findtamilore>
2024-08-28 11:37:14 +0100

    blocked a link

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -7,4 +7,21 @@ $(() => {
       $('section.cover').css('z-index', '-3');
     }, 10000);
   });
+
+
+  $('.start').on('click', () => {
+    $('.status.failed').css({
+      top: '20px',
+      display: 'flex'
+    });
+    $('.status.failed p').text('Sorry, starting a course is coming soon!');
+
+    setTimeout(() => {
+      $('.status.failed').fadeOut();
+      $('.status.failed').css({
+        top: '-70px',
+        display: 'none'
+      });
+    }, 5000);
+  });
 });
